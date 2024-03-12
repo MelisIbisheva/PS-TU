11f72784f17ea1db0d624f4c7fb34183f1f55fb8
Melis Ibisheva <93177517+MelisIbisheva@users.noreply.github.com>
2024-03-12 21:47:52 +0200

    Add files via upload

---
+++ PS_47_Melis/UI/Windows/AddUserWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using DataLayer.Database;
+using DataLayer.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Welcome.Others;
+
+namespace UI.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddUserWindow.xaml
+    /// </summary>
+    public partial class AddUserWindow : Window
+    {
+        public AddUserWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (DatabaseContext dbContext = new DatabaseContext())
+            {
+                if(Enum.TryParse<UserRolesEnum>(tbR.Text, out UserRolesEnum parsedRole))
+                {
+                    if (!UserExists(dbContext, tbN.Text))
+                    {
+
+                        dbContext.Add<DatabaseUser>(new DatabaseUser()
+                        {
+                            Name = tbN.Text,
+                            Password = tbP.Text,
+                            FNumber = tbFN.Text,
+                            Role = parsedRole,
+                            Email = tbEm.Text,
+                            Expires = DateTime.Now.AddYears(1)
+                        });
+                        dbContext.Logs.Add(new DatabaseLog()
+                        {
+                            Message = $"{tbN.Text} Created!",
+                        });
+                        dbContext.SaveChanges();
+                    }
+                    else
+                    {
+                        MessageBox.Show("User exist");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid role");
+                }
+
+
+            }
+        }
+        private bool UserExists(DatabaseContext dbContext, string username)
+        {
+            return dbContext.Users.Any(u => u.Name == username);
+        }
+    }
+}
---
+++ PS_47_Melis/UI/Windows/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UI.Windows;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LogsWindow lw = new LogsWindow();
+            lw.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            new RemoveUserWindow().Show();
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            new AddUserWindow().Show();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            studentsList.DisplayUsers();
+        }
+    }
+}
